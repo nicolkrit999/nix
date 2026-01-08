8ec5ebf4b22f2ba1e430bec71ab3360642c7ef16
nicolkrit999 <githubgitlabmain.hu5b7@passfwd.com>
2026-01-08 20:16:37 +0100

    changed ssddm theme to hyprland_kate

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -1,12 +1,20 @@
-{
-  pkgs,
-  lib,
-  vars,
-  ...
+{ pkgs
+, lib
+, vars
+, ...
 }:
 let
+  # Reference for themes:
+  # Files: https://github.com/Keyitdev/sddm-astronaut-theme/tree/master/themes
   sddmTheme = pkgs.sddm-astronaut.override {
-    embeddedTheme = "jake_the_dog";
+    embeddedTheme = "hyprland_kath"; # do not include ".conf" at the end
+    themeConfig = {
+      # Clock format. The default is 24/h format. If opting for the default these "themConfig" block can be removed
+      # "hh:mm AP" = 08:00 PM
+      # "HH:mm"    = 20:00
+      HourFormat = "hh:mm AP";
+      #DateFormat = ""; # "Some theme may not support this. Commmented because i like the default but kept for reference
+    };
   };
 in
 {
