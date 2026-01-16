ebce412e0403f7a3c74c4f69ac90890b2030cf50
nicolkrit999 <githubgitlabmain.hu5b7@passfwd.com>
2026-01-16 21:27:22 +0100

    personal improvementes

--- hosts/nixos-desktop/optional/general-hm-modules/default.nix
+++ hosts/nixos-desktop/optional/general-hm-modules/default.nix
@@ -2,7 +2,7 @@
 # Importing modules.nix is not necessary since flake.nix already handle it automatically
 {
   imports = [
-    ./web-apps.nix
+    ./pwa.nix
     ./home.nix
     #./wrappers.nix
   ];
