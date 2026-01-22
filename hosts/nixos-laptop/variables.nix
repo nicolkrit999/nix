d11e950d5509633eae909e55878e0b6ce57bc9e8
nicolkrit999 <githubgitlabmain.hu5b7@passfwd.com>
2026-01-23 00:15:19 +0100

    minor things for cachix

--- hosts/nixos-laptop/variables.nix
+++ hosts/nixos-laptop/variables.nix
@@ -136,7 +136,7 @@
   # Cachix
   cachix = {
     enable = true;
-    push = true; # Only the builder must have this true (for now "nixos-desktop")
+    push = false; # Only the builder must have this true (for now "nixos-desktop")
     name = "krit-nixos";
     # The public key from your screenshot:
     publicKey = "krit-nixos.cachix.org-1:54bU6/gPbvP4X+nu2apEx343noMoo3Jln8LzYfKD7ks=";
