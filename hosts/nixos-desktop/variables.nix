fbd57052a5dec6aefd3642598ee9623f4791ecab
nicolkrit999 <githubgitlabmain.hu5b7@passfwd.com>
2026-01-23 20:13:18 +0100

    fixed wallpaper fetching

--- hosts/nixos-desktop/variables.nix
+++ hosts/nixos-desktop/variables.nix
@@ -93,11 +93,9 @@
     }
 
     {
-      wallpaperURL =
-        "https://raw.githubusercontent.com/nicolkrit999/wallpaper-repo/main/various/various-websites/wallhaven/wallhaven-anime-girl-drink-4k-grey.png";
+      wallpaperURL = "https://raw.githubusercontent.com/nicolkrit999/dotfiles/main/wallpaper-repo/Pictures/wallpapers/various/various-websites/wallhaven/wallhaven-anime-girl-drink-4k-grey.png"
       wallpaperSHA256 = "0q5j531m3a1x5x99d0xybcb9rgc7w1i3v2vgf81zpwcwqj7abnzr";
     }
-
   ];
 
   # 🔋 POWER MANAGEMENT
