c011f1bc18522d0f0a0b80894ba65c21bf4482b3
nicolkrit999 <githubgitlabmain.hu5b7@passfwd.com>
2026-02-20 12:10:03 +0100

    boh

--- modules/toplevel/home-manager.nix
+++ modules/toplevel/home-manager.nix
@@ -7,10 +7,25 @@ delib.module {
     {
       imports = [ inputs.home-manager.nixosModules.default ];
 
+      # home-manager.nix
       home-manager = {
         useGlobalPkgs = false;
         useUserPackages = true;
-        extraSpecialArgs = { inherit inputs myconfig; };
+
+        # 🌟 THE FIX: Pass only the inputs you actually need for Home Manager.
+        # This prevents the 'armv5tel' crash from the shell flakes.
+        extraSpecialArgs = {
+          inherit myconfig;
+          inputs = {
+            inherit (inputs)
+              nixpkgs
+              home-manager
+              catppuccin
+              plasma-manager
+              ;
+          };
+        };
+
         sharedModules = [
           inputs.catppuccin.homeModules.catppuccin
           inputs.plasma-manager.homeModules.plasma-manager
