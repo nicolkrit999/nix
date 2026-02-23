90f3456b7fff6d89a6e2958a4349d8667908be33
nicolkrit999 <githubgitlabmain.hu5b7@passfwd.com>
2026-02-24 00:03:22 +0100

    trial for another hardware-configuration for nixos-laptop compatible with luks disko-config

---
+++ hosts/nixos-laptop/hardware-configuration.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  # Essential modules for ARM and NVMe/USB boot
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "usb_storage"
+    "xhci_pci"
+    "usbhid"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+}
