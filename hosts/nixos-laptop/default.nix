7139cca1e32a081f63ba4f7eb773bcdafd58c16f
nicolkrit999 <githubgitlabmain.hu5b7@passfwd.com>
2026-02-24 08:48:44 +0100

    changed locale to swiss italia except for main interface language

--- hosts/nixos-laptop/default.nix
+++ hosts/nixos-laptop/default.nix
@@ -554,7 +554,22 @@ delib.host {
         */
       ];
 
+
+      # Keep the main interface, terminal, and system language in English
       i18n.defaultLocale = "en_US.UTF-8";
+
+      # Override only the formats for numbers, dates, and measurements
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "it_CH.UTF-8"; # Address formatting
+        LC_IDENTIFICATION = "it_CH.UTF-8"; # Identification formatting for files (only metadata)
+        LC_MEASUREMENT = "it_CH.UTF-8"; # Uses Metric system (km, Celsius)
+        LC_MONETARY = "it_CH.UTF-8"; # Uses CHF formatting
+        LC_NAME = "it_CH.UTF-8"; # Personal name formatting (Surname Name)
+        LC_NUMERIC = "it_CH.UTF-8"; # Swiss number separators
+        LC_PAPER = "it_CH.UTF-8"; # Defaults printers to A4
+        LC_TELEPHONE = "it_CH.UTF-8"; # Telephone number formatting (e.g. +41 79 123 45 67)
+        LC_TIME = "it_CH.UTF-8"; # 24-hour clock and DD.MM.YYYY
+      };
 
       # TODO: Enable when host sops secrets are configured
       /*
