9931381d78fbe7c6b1ef29c3e5628ecc2f003567
nicolkrit999 <githubgitlabmain.hu5b7@passfwd.com>
2026-02-24 00:11:52 +0000

    added walker substituter

--- modules/toplevel/nix.nix
+++ modules/toplevel/nix.nix
@@ -13,10 +13,12 @@ delib.module {
       extra-substituters = [
         "https://hyprland.cachix.org"
         "https://cosmic.cachix.org"
+        "https://walker.cachix.org"
       ];
       extra-trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+        "walker.cachix.org-1:f696P6Sgzif6Gf29v6f1v6f6f6f6f6f6f6f6f6f6f6f="
       ];
     };
     nix.gc.automatic = false;
