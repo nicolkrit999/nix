55e9bdf7a57aaae86f14b708c88df70056d9d849
Krit Pio Nicol <githubgitlabmain.hu5b7@passfwd.com>
2026-03-14 23:47:17 +0100

    put back claude plugins as global git ignore

--- modules/programs/claude-code.nix
+++ modules/programs/claude-code.nix
@@ -24,6 +24,7 @@ delib.module {
     ];
 
     environment.shellAliases = {
+      caiw = "claude --worktree";
       caitempplugins = "npx claude-code-templates@latest --plugins";
       caitemphealt = "npx claude-code-templates@latest --health-check";
       caitempchat = "npx claude-code-templates@latest --chats";
