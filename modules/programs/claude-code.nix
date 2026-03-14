59986d9a524b0080eab8fde64f5e00c88ba8dbdb
Krit Pio Nicol <githubgitlabmain.hu5b7@passfwd.com>
2026-03-14 22:56:15 +0100

    added claude code api openrouter key to sops secrets

--- modules/programs/claude-code.nix
+++ modules/programs/claude-code.nix
@@ -12,10 +12,18 @@ delib.module {
     services.claude-cowork.package = inputs.claude-cowork-service.packages.${pkgs.stdenv.hostPlatform.system}.claude-cowork-service; # Temporary to fix evaluation warning about "'system' has been renamed to/replaced by 'stdenv.hostPlatform.system'"
     nixpkgs.overlays = [ inputs.claude-code.overlays.default ];
 
-    environment.systemPackages = [ pkgs.claude-code ];
+    environment.systemPackages = [
+      pkgs.claude-code
+
+      (pkgs.writeShellScriptBin "cai" ''
+        if [ -f /run/secrets/openrouter_api_claude_code ]; then
+          export OPENROUTER_API_KEY=$(cat /run/secrets/openrouter_api_claude_code)
+        fi
+        exec ${pkgs.claude-code}/bin/claude "$@"
+      '')
+    ];
 
     environment.shellAliases = {
-      cai = "claude";
       caitempplugins = "npx claude-code-templates@latest --plugins";
       caitemphealt = "npx claude-code-templates@latest --health-check";
       caitempchat = "npx claude-code-templates@latest --chats";
