8de9cba6a80e0245c1fb8c116a472af10aa4791b
Krit Pio Nicol <githubgitlabmain.hu5b7@passfwd.com>
2026-04-16 19:02:33 +0200

    Momentary: Changed kernel.nix to force nixos-laptop on testing (7.0)
    Any other x86 host remains on zen. aarch64-linux and aarc64-darwin stay
    on latest

--- modules/nixos/toplevel/kernel.nix
+++ modules/nixos/toplevel/kernel.nix
@@ -1,10 +1,11 @@
 { delib, pkgs, ... }:
 delib.module {
   name = "kernel";
-  nixos.always = {
-    # Use Zen kernel on x86 (Desktop), but standard Linux on ARM (VM/Pi/Apple)
+  nixos.always = { myconfig, ... }: {
     boot.kernelPackages =
-      if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then
+      if myconfig.constants.hostname == "nixos-laptop" then
+        pkgs.linuxPackages_testing
+      else if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then
         pkgs.linuxPackages_zen
       else
         pkgs.linuxPackages_latest;
