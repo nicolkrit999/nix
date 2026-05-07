453a99a117089bcc9cb7c627993cf1d0ac0e7cf9
Krit Pio Nicol <githubgitlabmain.hu5b7@passfwd.com>
2026-05-07 19:54:29 +0200

    added moz policy to zen browser

--- users/krit/nixos/programs/gui-programs/zen-browser/settings.nix
+++ users/krit/nixos/programs/gui-programs/zen-browser/settings.nix
@@ -13,6 +13,7 @@ delib.module {
 
       profiles.default.settings = {
         "zen.view.compact.hide-tabbar" = true;
+
         "zen.view.compact.enable-at-startup" = false;
         "zen.urlbar.behavior" = "float";
 
@@ -31,6 +32,47 @@ delib.module {
         "signon.autofillForms" = false;
         "signon.generation.enabled" = false;
         "signon.management.page.breach-alerts.enabled" = false;
+
+        "browser.uiCustomization.state" = builtins.toJSON {
+          placements = {
+            widget-overflow-fixed-list = [ ];
+            unified-extensions-area = [
+              "sponsorblocker_ajay_app-browser-action"
+              "ublock0_raymondhill_net-browser-action"
+              "jid0-gxjllfbcoax0lcltedfrekqdqpi_jetpack-browser-action"
+              "firefoxcolor_mozilla_com-browser-action"
+              "_506e023c-7f2b-40a3-8066-bc5deb40aebe_-browser-action"
+              "search_kagi_com-browser-action"
+              "_d8b32864-153d-47fb-93ea-c273c4d1ef17_-browser-action"
+            ];
+            nav-bar = [
+              "back-button"
+              "forward-button"
+              "stop-reload-button"
+              "customizableui-special-spring1"
+              "vertical-spacer"
+              "urlbar-container"
+              "customizableui-special-spring2"
+              "jid1-mnnxcxisbpnsxq_jetpack-browser-action"
+              "extension_one-tab_com-browser-action"
+              "_c0e1baea-b4cb-4b62-97f0-278392ff8c37_-browser-action"
+              "unified-extensions-button"
+              "78272b6fa58f4a1abaac99321d503a20_proton_me-browser-action"
+              "addon_simplelogin-browser-action"
+            ];
+            toolbar-menubar = [ "menubar-items" ];
+            TabsToolbar = [ "tabbrowser-tabs" ];
+            vertical-tabs = [ ];
+            PersonalToolbar = [ "import-button" "personal-bookmarks" ];
+            zen-sidebar-top-buttons = [ "zen-toggle-compact-mode" ];
+            zen-sidebar-foot-buttons = [
+              "downloads-button"
+              "zen-workspaces-button"
+              "zen-create-new-button"
+            ];
+          };
+          currentVersion = 23;
+        };
       };
     };
   };
