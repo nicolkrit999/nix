c14cef0b2253aa54f507f39f4d86558f354d026b
Krit Pio Nicol <githubgitlabmain.hu5b7@passfwd.com>
2026-05-18 22:01:18 +0200

    changed walker and vicinae trusted public keys

--- modules/nixos/toplevel/nix-nixos.nix
+++ modules/nixos/toplevel/nix-nixos.nix
@@ -34,9 +34,9 @@ delib.module {
       extra-trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-        "walker.cachix.org-1:f696P6Sgzif6Gf29v6f1v6f6f6f6f6f6f6f6f6f6f6f="
+        "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
         "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="
-        "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcN3w4="
+        "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
       ];
     };
     nix.gc.automatic = false;
